56e2a04f16391c9f5a7e5d475363eb0c96384017
chachacharlie <carlos.torres@scisa.com>
2025-06-24 11:52:37 -0600

    ADD: Fixing search records by name

--- schemas/rework_rate/rework_rate_query.py
+++ schemas/rework_rate/rework_rate_query.py
@@ -2,12 +2,17 @@ from sqlalchemy.orm import Session, joinedload
 import strawberry
 from models.rework import ReworkDataDB, rework_data_tags
 from models.tags import TagDB
-from schemas.rework_rate.rework_rate_types import ReworkDataType, RepoUrlType, MeanAndMedianType
+from schemas.rework_rate.rework_rate_types import (
+    ReworkDataType,
+    RepoUrlType,
+    MeanAndMedianType,
+)
 from resolvers.rework import convert_to_type
 from core.utils.formatter import extract_repo_name
 from typing import Optional
 from datetime import datetime
 from sqlalchemy import and_, func
+
 
 @strawberry.type
 class Query:
@@ -17,24 +22,30 @@ class Query:
         records = db.query(ReworkDataDB).all()
         return [convert_to_type(record) for record in records]
 
-
     @strawberry.field
     def get_rework_data_by_name(
-        self,
-        info,
-        repo_name: Optional[str] = None,
-        tags: Optional[list[str]] = None
+        self, info, repo_url: Optional[str] = None, tags: Optional[list[str]] = None
     ) -> list[ReworkDataType]:
         db: Session = info.context["db"]
 
-        print(f"Filtering by repo_name: {repo_name}, tags: {tags}")
         query = db.query(ReworkDataDB)
-        if (not repo_name and not tags) or (repo_name == "" and (not tags or len(tags) == 0)):
-            records = query.all()
+
+        # Si no hay filtros, retorna todo
+        if (not repo_url and not tags) or (
+            repo_url == "" and (not tags or len(tags) == 0)
+        ):
+            records = query.options(joinedload(ReworkDataDB.tags)).all()
             return [convert_to_type(record) for record in records]
 
-        if repo_name:
-            query = query.filter(ReworkDataDB.repo_url.ilike(f"%{repo_name}%"))
+        if repo_url:
+            print(f"Filtering by repo_url: {repo_url}")
+            query = query.filter(
+                func.right(
+                    ReworkDataDB.repo_url,
+                    func.charindex("/", func.reverse(ReworkDataDB.repo_url)) - 1,
+                ) == repo_url
+            )
+            records = query.all()
 
         if tags and len(tags) > 0:
             subq = (
@@ -48,8 +59,8 @@ class Query:
             )
             query = query.filter(subq)
 
+        
         query = query.options(joinedload(ReworkDataDB.tags))
-
         records = query.all()
 
         return [convert_to_type(record) for record in records]
@@ -57,7 +68,9 @@ class Query:
     @strawberry.field
     def get_rework_data_by_pr(self, info, pr_number: str) -> ReworkDataType:
         db: Session = info.context["db"]
-        record = db.query(ReworkDataDB).filter(ReworkDataDB.pr_number == pr_number).first()
+        record = (
+            db.query(ReworkDataDB).filter(ReworkDataDB.pr_number == pr_number).first()
+        )
         return convert_to_type(record) if record else None
 
     @strawberry.field
@@ -79,10 +92,10 @@ class Query:
         info,
         repo_url: str,
         start_date: Optional[datetime] = None,
-        end_date: Optional[datetime] = None
+        end_date: Optional[datetime] = None,
     ) -> list[ReworkDataType]:
         db: Session = info.context["db"]
-        
+
         query = db.query(ReworkDataDB).filter(ReworkDataDB.repo_url == repo_url)
 
         if start_date:
@@ -94,16 +107,16 @@ class Query:
             query = query.filter(
                 and_(
                     ReworkDataDB.createdAtDate >= start_date,
-                    ReworkDataDB.createdAtDate <= end_date
+                    ReworkDataDB.createdAtDate <= end_date,
                 )
             )
         query = query.order_by(ReworkDataDB.period_start.asc())
         records = query.all()
         return [convert_to_type(record) for record in records]
-    
+
     @strawberry.field
     def get_mean_and_median(
-        self, 
+        self,
         info,
         repo_url: str,
         start_date: Optional[datetime] = None,
@@ -112,29 +125,29 @@ class Query:
         db: Session = info.context["db"]
 
         query = db.query(ReworkDataDB).filter(ReworkDataDB.repo_url == repo_url)
-        
+
         # Apply date filters if provided
         if start_date and end_date:
             query = query.filter(
                 and_(
                     ReworkDataDB.period_start >= start_date,
-                    ReworkDataDB.period_start <= end_date
+                    ReworkDataDB.period_start <= end_date,
                 )
             )
         # Get all records for the specified repo_url and date range
         records = query.all()
         if not records:
             return MeanAndMedianType(mean=0.0, median=0.0)
-        
+
         # Calculate mean and median of rework percentages
         rework_percentages = [record.rework_percentage for record in records]
         mean = sum(rework_percentages) / len(rework_percentages)
-        
+
         sorted_percentages = sorted(rework_percentages)
         n = len(sorted_percentages)
         if n % 2 == 0:
             median = (sorted_percentages[n // 2 - 1] + sorted_percentages[n // 2]) / 2
         else:
             median = sorted_percentages[n // 2]
-        
-        return MeanAndMedianType(mean=mean, median=median) 
+
+        return MeanAndMedianType(mean=mean, median=median)
