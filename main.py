de919fcb080a6ac7ec2afb8f47f8510d454749a4
chachacharlie <carlos.torres@scisa.com>
2025-07-18 09:30:14 -0600

    Change: Update Uvicorn port to 8000 and remove unused import in repository query

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ app.include_router(graphql_app, prefix="/graphql")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- schemas/repository/repository_query.py
+++ schemas/repository/repository_query.py
@@ -5,7 +5,6 @@ from typing import List, Optional
 from strawberry.types import Info
 from models.repository import RepositoryEntity
 from schemas.repository.repository_type import RepositoryType
-from schemas.tags.tags_types import TagType
 
 from repositories.repository_repository import RepositoryRepository
 from services.repository_service import RepositoryService
@@ -16,7 +15,6 @@ class RepositoryQuery:
     @strawberry.field
     def get_all_repositories(self, info, filters: Optional[RepositoryFilterInput]) -> List[RepositoryType]:
         db: Session = info.context["db"]
-        
         repo = RepositoryRepository(RepositoryEntity, db)
         service = RepositoryService(db, repo)
 
